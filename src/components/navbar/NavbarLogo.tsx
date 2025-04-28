3d7c5afd2f1e37c271633aa6a1827f9ce7321721
Arpita Behura <arpitabehura.009@gmail.com>
2025-04-28 15:40:10 +0200

    Navbar comp completion

--- src/components/navbar/NavbarLogo.tsx
+++ src/components/navbar/NavbarLogo.tsx
@@ -1,7 +1,8 @@
 const NavbarLogo = () => {
     return (
         <div>
-            NavbarLogo
+            <h1 className="text-white text-2xl sm:hidden md:block">Arpita Behura</h1>
+            <h1 className="text-white font-special font-extrabold text-4xl sm:block md:hidden">AB</h1>
         </div>
     );
 };
