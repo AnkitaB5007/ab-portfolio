ce2378c1b784c6d96c1d7c04fb699e6d3f1436cc
Arpita Behura <arpitabehura.009@gmail.com>
2025-04-30 15:11:49 +0200

    Some bug fixes

--- src/components/heroSection/HeroText.tsx
+++ src/components/heroSection/HeroText.tsx
@@ -13,7 +13,7 @@ export const words: WordItem[] = [
 
 const HeroText = () => {
     return (
-        <div className="flex flex-col gap-4 h-full justify-center md:text-left sm:text-center">
+        <div className="flex w-[40%] flex-col gap-4 h-full justify-center md:text-left sm:text-center">
             <motion.h2 className="lg:text-2xl sm:text-xl uppercase text-cyan"
                        variants={fadeIn('down',0.2)}
                        initial='hidden'
@@ -39,7 +39,7 @@ const HeroText = () => {
                 <h1 className="md:text-[2.7rem] lg:text-5xl sm:text-3xl mt-4 font-bold font-special text-white">Bringing
                     <span className="slide">
                         <span className="wrapper">
-                            {words.map(word => <span key={word.text} className="flex items-center md:gap-4 gap:3 pb:2">
+                            {words.map(word => <span key={word.text} className="flex items-center md:gap-3 gap:2 pb-[3px]">
                                 <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full white-bg"/>
                                 <span>{word.text}</span>
                             </span>)}
