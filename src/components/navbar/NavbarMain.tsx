e6f99cb57cc1eb571cc7fa6706d1c099ac9277d1
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-01 23:47:00 +0200

    Localization for navBar section

--- src/components/navbar/NavbarMain.tsx
+++ src/components/navbar/NavbarMain.tsx
@@ -8,7 +8,7 @@ import { requiredLanguages } from "../../constants/languageSwitcherData";
 import { useTranslation } from "react-i18next";
 
 const NavbarMain = () => {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const [menuOpen, setMenuOpen] = useState(false);
   const [selectedLanguageCode, setSelectedLanguageCode] = useState("en");
   const toggleMenu = () => {
@@ -23,7 +23,6 @@ const NavbarMain = () => {
     <nav className="max-w-[1300px] mx-auto w-full  px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
       <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black items-center p-6 rounded-r-full rounded-l-full border-orange border-[0.5px]">
         <NavbarLogo />
-        <p className="text-white">{t("designation")}</p>
         <LanguageSwitcher
           requiredLanguages={requiredLanguages} // Ensure this prop is defined
           onSelectionChange={handleLanguageChange}
