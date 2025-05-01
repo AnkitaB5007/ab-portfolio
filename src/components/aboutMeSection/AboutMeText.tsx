e1c3c327adbdc8af382e7d02255c8c2352f5ab92
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-02 01:36:37 +0200

    AboutMe Section localization

--- src/components/aboutMeSection/AboutMeText.tsx
+++ src/components/aboutMeSection/AboutMeText.tsx
@@ -1,26 +1,32 @@
-import {connectText, shortIntro} from "../../constants/aboutMeData";
+import { connectText, shortIntro } from "../../constants/aboutMeData";
 import { Link } from "react-scroll";
+import { useTranslation } from "react-i18next";
 
 const AboutMeText = () => {
-    return (
-        <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
-            <h2 className="text-6xl text-cyan mb-10">About Me</h2>
-            <p className="text-white text-xl">{shortIntro}</p>
-            <p className="mt-1 text-white text-xl">{connectText}</p>
-            <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
-                <Link
-                    spy={true}
-                    smooth={true}
-                    duration={500}
-                    offset={-120}
-                    to="projects"
-                    className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
-                >
-                    My Projects
-                </Link>
-            </button>
-        </div>
-    );
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col md:items-start sm:items-center md:text-left sm:text-center">
+      <h2 className="text-6xl text-cyan mb-10">
+        {t("aboutMeSection.aboutMe_text")}
+      </h2>
+      <p className="text-white text-xl">{t(`aboutMeSection.${shortIntro}`)}</p>
+      <p className="mt-1 text-white text-xl">
+        {t(`aboutMeSection.${connectText}`)}
+      </p>
+      <button className="border border-orange rounded-full py-2 px-4 text-lg flex gap-2 items-center mt-10 hover:bg-orange transition-all duration-500 cursor-pointer md:self-start sm:self-center">
+        <Link
+          spy={true}
+          smooth={true}
+          duration={500}
+          offset={-120}
+          to="projects"
+          className="cursor-pointer text-white hover:text-cyan transition-all duration-500"
+        >
+          {t("aboutMeSection.aboutMe_btn_projects")}
+        </Link>
+      </button>
+    </div>
+  );
 };
 
 export default AboutMeText;
--- src/constants/aboutMeData.ts
+++ src/constants/aboutMeData.ts
@@ -1,8 +1,3 @@
-export const shortIntro = 'I\'m Arpita, a web developer on a mission to make the internet a bit more delightful—one line of code\n' +
-    '                at a time! Armed with a keyboard, creativity, and a healthy appetite\n' +
-    '                for pizza, I thrive in the world of pixels and programming while\n' +
-    '                continuously learning and exploring. I believe that laughter can tackle any tricky bug,\n' +
-    '                and I aim to create websites that leave users grinning.';
+export const shortIntro = "aboutMe_text_intro";
 
-export const connectText ='Let’s team up and craft something\n' +
-    '                exceptional together, all while keeping the fun alive!';
+export const connectText = "aboutMe_text_connect";
