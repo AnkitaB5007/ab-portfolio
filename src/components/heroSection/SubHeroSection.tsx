32527b99022e4d9bf19e128ce77a1f26b3d55054
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-02 01:27:53 +0200

    sub hero section localization added

--- src/components/heroSection/SubHeroSection.tsx
+++ src/components/heroSection/SubHeroSection.tsx
@@ -1,11 +1,18 @@
+import { useTranslation } from "react-i18next";
+
 const SubHeroSection = () => {
-    return (
-        <div className="w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 items-center gap-4 bg-brown">
-            <p className="md:block sm:hidden">Fast Learner</p>
-            <p className="md:block sm:hidden">Team Work</p>
-            <p>Details Master</p>
-        </div>
-    );
+  const { t } = useTranslation();
+  return (
+    <div className="w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 items-center gap-4 bg-brown">
+      <p className="md:block sm:hidden">
+        {t("heroSection.hero_text_subHero_1")}
+      </p>
+      <p className="md:block sm:hidden">
+        {t("heroSection.hero_text_subHero_2")}
+      </p>
+      <p>{t("heroSection.hero_text_subHero_3")}</p>
+    </div>
+  );
 };
 
 export default SubHeroSection;
