4e95389ea567915be7ffc17ecd0525c1942c4976
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-02 13:44:40 +0200

    All Experiences section localization

--- src/components/experienceSection/AllExperiences.tsx
+++ src/components/experienceSection/AllExperiences.tsx
@@ -1,18 +1,24 @@
 import SingleExperience from "./SingleExperience";
-import {experiences} from "../../constants/experienceData";
+import { useTranslation } from "react-i18next";
+import { Experience } from "../../constants/experienceData";
 
 const AllExperiences = () => {
-    return (
-        <div className="flex flex-wrap md:flex-row sm:flex-col items-center justify-between">
-            {experiences.map((experience, index) => {
-                return(
-                    <>
-                        <SingleExperience key={index} experience={experience} />
-                    </>
-                )
-            })}
-        </div>
-    );
+  const { t } = useTranslation();
+  const experiences = t("experienceSection.experienceAllYearCategorization", {
+    returnObjects: true,
+  }) as Experience;
+
+  return (
+    <div className="flex flex-wrap md:flex-row sm:flex-col items-center justify-between">
+      {experiences.map((experience: Experience, index: number) => {
+        return (
+          <>
+            <SingleExperience key={index} experience={experience} />
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AllExperiences;
--- src/constants/experienceData.ts
+++ src/constants/experienceData.ts
@@ -1,61 +1,7 @@
 export interface Experience {
-    job: string;
-    company: string;
-    location: string;
-    date: string;
-    responsibilities: string[];
-};
-export const experiences: Experience[] = [
-    {
-        job: "Full-Stack Developer",
-        company: "COMECO GmbH & Co. KG",
-        location: "Stuttgart",
-        date: "2024 - Present",
-        responsibilities: [
-            "Building user-facing features using JavaScript frameworks",
-            "Creating responsive, mobile-friendly designs",
-            "Managing component states",
-            "Developing server-side logic with Node.js",
-            "Handling API development",
-            "Managing Databases",
-            "Testing & Debugging",
-        ],
-    },
-    {
-        job: "Front-End Developer",
-        company: "COMECO GmbH & Co. KG",
-        location: "Stuttgart",
-        date: "2022 - 2023",
-        responsibilities: [
-            "Implementing reusable components",
-            "Fetching and displaying data from backend APIs",
-            "Participating in large scale application",
-            "Working on the performance of web applications",
-            "Generating new ideas for better user experience",
-        ],
-    },
-    {
-        job: "Software Developer",
-        company: "Cognizant Technology Solutions",
-        location: "India",
-        date: "2018 - 2019",
-        responsibilities: [
-            "Developing responsive, accessible, and cross-browser compatible web pages",
-            "Translating UI/UX designs (often from Figma or Adobe XD) into fully functional web components and layouts",
-            "Identifying issues reported by QA or users and fix frontend bugs",
-        ],
-    },
-    {
-        job: "Senior Test Analyst",
-        company: "Cognizant Technology Solutions",
-        location: "India",
-        date: "2018 - 2019",
-        responsibilities: [
-            "ETL Testing",
-            "Identified business flows and defined Test Strategy",
-            "Developed test cases",
-            "Tracked bugs/defects",
-            "Data comparison between source & target",
-        ],
-    },
-];
+  job: string;
+  company: string;
+  location: string;
+  date: string;
+  responsibilities: string[];
+}
