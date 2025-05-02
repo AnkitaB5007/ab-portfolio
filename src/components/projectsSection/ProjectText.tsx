6b5a3e231e2d93e82397fb9a094bd5d82c4ac133
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-02 14:36:13 +0200

    Projects section localization

--- src/components/projectsSection/ProjectText.tsx
+++ src/components/projectsSection/ProjectText.tsx
@@ -1,14 +1,17 @@
+import { useTranslation } from "react-i18next";
+
 const ProjectsText = () => {
-    return (
-        <div className="flex flex-col items-center mt-[100px]">
-            <h2 className="text-6xl text-cyan mb-10">Projects</h2>
-            <p className="text-lg text-center text-white">
-                I have worked on a variety of web development projects, ranging from
-                responsive websites for small businesses to full-stack applications and
-                complex front-end interfaces.
-            </p>
-        </div>
-    );
+  const { t } = useTranslation();
+  return (
+    <div className="flex flex-col items-center mt-[100px]">
+      <h2 className="text-6xl text-cyan mb-10">
+        {t("projectsSection.projects_text")}
+      </h2>
+      <p className="text-lg text-center text-white">
+        {t("projectsSection.projects_shortDesc")}
+      </p>
+    </div>
+  );
 };
 
 export default ProjectsText;
