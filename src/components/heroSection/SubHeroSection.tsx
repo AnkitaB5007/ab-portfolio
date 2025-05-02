a0551f298f0f7f66fbbca126c1648a5dd46aaa59
Arpita Behura <arpitabehura.009@gmail.com>
2025-05-02 21:17:06 +0200

    Subhero section for small devices

--- src/components/heroSection/SubHeroSection.tsx
+++ src/components/heroSection/SubHeroSection.tsx
@@ -4,13 +4,10 @@ const SubHeroSection = () => {
   const { t } = useTranslation();
   return (
     <div className="w-full border-y border-lightGrey text-lightGrey flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-4xl py-8 items-center gap-4 bg-brown">
-      <p className="md:block sm:hidden">
-        {t("heroSection.hero_text_subHero_1")}
-      </p>
-      <p className="md:block sm:hidden">
-        {t("heroSection.hero_text_subHero_2")}
-      </p>
-      <p>{t("heroSection.hero_text_subHero_3")}</p>
+      <p className="hidden md:block">{t("heroSection.hero_text_subHero_1")}</p>
+      <p className="hidden md:block">{t("heroSection.hero_text_subHero_2")}</p>
+      <p className="block md:hidden">{t("heroSection.hero_text_subHero_3")}</p>
+      <p className="hidden md:block">{t("heroSection.hero_text_subHero_3")}</p>
     </div>
   );
 };
